1d5cbf940c9f57d70660fcc2daae129e6f9efff9
regisfoucault <regisfoucault@gmail.com>
2013-05-01 09:40:53 +0200

    Add: code cleaning

--- site.hs
+++ site.hs
@@ -4,33 +4,27 @@ import           Control.Applicative ((<$>))
 import           Data.Monoid         (mappend)
 import           Hakyll
 --------------------------------------------------------------------------------
+
 main :: IO ()
 main = hakyll $ do
-    match "assets/images/*" $ do
+
+    -- Copy files and images
+    match ("assets/images/*" .||. "assets/js/*" .||. "assets/font/*" .||. "assets/fancybox/*") $ do
         route   idRoute
         compile copyFileCompiler
 
-    match "assets/js/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-
-    match "assets/font/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-
-    match "assets/fancybox/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-
+    -- Compress css
     match "assets/css/*.css" $ do
         route   idRoute
         compile compressCssCompiler
 
+    -- Compile less
     match "assets/css/*.less" $ do
         route   $ setExtension "css"
         compile $ getResourceString >>=
             withItemBody (unixFilter "lessc" ["-","--yui-compress","-O2"])
 
+    -- Render posts
     match "posts/*/*" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
@@ -41,7 +35,7 @@ main = hakyll $ do
     match "index.html" $ do
         route idRoute
         compile $ do
-            let indexCtx = field "posts" $ \_ -> postList (take 3 . recentFirst)
+          let indexCtx = field "posts" $ \_ -> postList (take 3 . recentFirst)
 
             getResourceBody
                 >>= applyAsTemplate indexCtx
