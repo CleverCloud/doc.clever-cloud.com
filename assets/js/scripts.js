e9c59576307de75445f3958788306af4e29acf29
regisfoucault <regisfoucault@gmail.com>
2013-06-12 23:28:29 +0200

    indentation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -47,17 +47,16 @@ $(document).ready(function() {
 
   // Show or hide the sticky footer button
   $(window).scroll(function() {
-  if ($(this).scrollTop() > 200) {
-    $('.cc-go-top').fadeIn(200);
-  } else {
-    $('.cc-go-top').fadeOut(200);
-  }
-});
+    if ($(this).scrollTop() > 200) {
+      $('.cc-go-top').fadeIn(200);
+    } else {
+      $('.cc-go-top').fadeOut(200);
+    }
+  });
 
-// Animate the scroll to top
-$('.cc-go-top').click(function(event) {
-  event.preventDefault();
-  
-  $('html, body').animate({scrollTop: 0}, 300);
-})
+  // Animate the scroll to top
+  $('.cc-go-top').click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({scrollTop: 0}, 300);
+  })
 });
